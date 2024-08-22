58e19507ddc07eb4c95abfee75fa1a44f935f469
crossovka <best-login-mail@yandex.ru>
2024-08-23 00:27:37 +0900

    close sort when click outside

--- src/components/Sort.jsx
+++ src/components/Sort.jsx
@@ -1,11 +1,12 @@
-import { useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { setActiveSortType } from '../redux/slices/filterSlice';
 
 const Sort = () => {
 	const dispatch = useDispatch();
 	const sortOptions = useSelector((state) => state.filterSlice.sortOptions);
-	const [open, setIsOpen] = useState(false);
+	const sortRef = useRef();
+	const [isOpen, setIsOpen] = useState(false);
 
 	const options = [
 		{ name: 'популярности (Desc)', sortProperty: '-rating' },
@@ -25,8 +26,19 @@ const Sort = () => {
 		}
 	};
 
+	const handleClickOutside = (e) => {
+		if (sortRef.current && !sortRef.current.contains(e.target)) {
+			setIsOpen(false);
+		}
+	};
+
+	useEffect(() => {
+		document.body.addEventListener('click', handleClickOutside, true);
+		return () => document.body.removeEventListener('click', handleClickOutside, true);
+	}, []);
+
 	return (
-		<div className="sort">
+		<div ref={sortRef} className="sort">
 			<div className="sort__label">
 				<svg
 					width="10"
@@ -34,7 +46,7 @@ const Sort = () => {
 					viewBox="0 0 10 6"
 					fill="none"
 					xmlns="http://www.w3.org/2000/svg"
-					className={open ? 'rotated' : ''}
+					className={isOpen ? 'rotated' : ''}
 				>
 					<path
 						d="M10 5C10 5.16927 9.93815 5.31576 9.81445 5.43945C9.69075 5.56315 9.54427 5.625 9.375 5.625H0.625C0.455729 5.625 0.309245 5.56315 0.185547 5.43945C0.061849 5.31576 0 5.16927 0 5C0 4.83073 0.061849 4.68424 0.185547 4.56055L4.56055 0.185547C4.68424 0.061849 4.83073 0 5 0C5.16927 0 5.31576 0.061849 5.43945 0.185547L9.81445 4.56055C9.93815 4.68424 10 4.83073 10 5Z"
@@ -43,8 +55,8 @@ const Sort = () => {
 				</svg>
 				<b>Сортировка по:</b>
 				<div className="sort__popup-wrap">
-					<span onClick={() => setIsOpen(!open)}>{sortOptions.name}</span>
-					<div className={`sort__popup ${open ? 'active' : ''}`}>
+					<span onClick={() => setIsOpen(!isOpen)}>{sortOptions.name}</span>
+					<div className={`sort__popup ${isOpen ? 'active' : ''}`}>
 						<ul>
 							{options.map((obj) => (
 								<li
